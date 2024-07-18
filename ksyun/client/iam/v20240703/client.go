bd84e93b72acc9497cabbb7d09d00fff7f020a1e
fengyikai <fengyikai@kingsoft.com>
2024-07-19 00:03:39 +0800

    Update SDK

---
+++ ksyun/client/iam/v20240703/client.go
@@ -0,0 +1,58 @@
+package v20240703
+import (
+    "context"
+    "fmt"
+    "github.com/kingsoftcloud/sdk-go/ksyun/common"
+    ksyunhttp "github.com/kingsoftcloud/sdk-go/ksyun/common/http"
+    "github.com/kingsoftcloud/sdk-go/ksyun/common/profile"
+)
+
+const APIVersion = "2024-07-03"
+
+type Client struct {
+    common.Client
+}
+
+func NewClient(credential common.Credentials, region string, clientProfile *profile.ClientProfile) (client *Client, err error) {
+    client = &Client{}
+    client.Init(region).
+        WithCredential(credential).
+        WithProfile(clientProfile)
+    return
+}
+
+func NewProjectsInfoByInstanceIdsRequest() (request *ProjectsInfoByInstanceIdsRequest) {
+    request = &ProjectsInfoByInstanceIdsRequest{
+        BaseRequest: &ksyunhttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("iam", APIVersion, "ProjectsInfoByInstanceIds")
+    return
+}
+
+func NewProjectsInfoByInstanceIdsResponse() (response *ProjectsInfoByInstanceIdsResponse) {
+    response = &ProjectsInfoByInstanceIdsResponse{
+        BaseResponse: &ksyunhttp.BaseResponse{},
+    }
+    return
+}
+
+func (c *Client) ProjectsInfoByInstanceIds(request *ProjectsInfoByInstanceIdsRequest) (string) {
+    return c.ProjectsInfoByInstanceIdsWithContext(context.Background(), request)
+}
+
+func (c *Client) ProjectsInfoByInstanceIdsWithContext(ctx context.Context, request *ProjectsInfoByInstanceIdsRequest) (string) {
+    if request == nil {
+        request = NewProjectsInfoByInstanceIdsRequest()
+    }
+    request.SetContext(ctx)
+    request.SetContentType("application/x-www-form-urlencoded")
+
+    response := NewProjectsInfoByInstanceIdsResponse()
+    err, msg := c.Send(request, response)
+    if err != nil {
+        return fmt.Sprintf("%+v\n", err)
+    }
+    return msg
+}
+
+
---
+++ ksyun/client/iam/v20240703/models.go
@@ -0,0 +1,52 @@
+package v20240703
+import (
+	"encoding/json"
+    "github.com/kingsoftcloud/sdk-go/ksyun/common/errors"
+    ksyunhttp "github.com/kingsoftcloud/sdk-go/ksyun/common/http"
+)
+
+type ProjectsInfoByInstanceIdsRequest struct {
+    *ksyunhttp.BaseRequest
+    InstanceIds *string `json:"InstanceIds,omitempty" name:"InstanceIds"`
+}
+
+func (r *ProjectsInfoByInstanceIdsRequest) ToJsonString() string {
+    b, _ := json.Marshal(r)
+    return string(b)
+}
+
+func (r *ProjectsInfoByInstanceIdsRequest) FromJsonString(s string) error {
+    f := make(map[string]interface{})
+    if err := json.Unmarshal([]byte(s), &f); err != nil {
+        return err
+    }
+    if len(f) > 0 {
+        return errors.NewKsyunSDKError("ClientError.BuildRequestError", "ProjectsInfoByInstanceIdsRequest has unknown keys!", "")
+    }
+    return json.Unmarshal([]byte(s), &r)
+}
+
+type ProjectsInfoByInstanceIdsResponse struct {
+    *ksyunhttp.BaseResponse
+	List []struct {
+		ProductLine *string `json:"ProductLine"`
+		InstanceId *string `json:"InstanceId"`
+		InstanceName *string `json:"InstanceName"`
+		Region *string `json:"Region"`
+		ProjectId *int `json:"ProjectId"`
+		ProjectName *string `json:"ProjectName"`
+		ProjectDesc *string `json:"ProjectDesc"`
+	} `json:"List"`
+    Total *int `json:"Total" name:"Total"`
+    RequestId *string `json:"RequestId" name:"RequestId"`
+}
+
+func (r *ProjectsInfoByInstanceIdsResponse) ToJsonString() string {
+    b, _ := json.Marshal(r)
+    return string(b)
+}
+
+func (r *ProjectsInfoByInstanceIdsResponse) FromJsonString(s string) error {
+    return json.Unmarshal([]byte(s), &r)
+}
+
