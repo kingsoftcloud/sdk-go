4d424d54724d7d1e25ead0ba5eac8c45599c9da3
fengyikai <fengyikai@kingsoft.com>
2025-03-09 00:42:12 +0800

    Update SDK

--- ksyun/client/sts/v20151101/client.go
+++ ksyun/client/sts/v20151101/client.go
@@ -3,9 +3,9 @@ package v20151101
 import (
 	"context"
 	"fmt"
-	"github.com/kingsoftcloud/sdk-go/v2/ksyun/common"
-	ksyunhttp "github.com/kingsoftcloud/sdk-go/v2/ksyun/common/http"
-	"github.com/kingsoftcloud/sdk-go/v2/ksyun/common/profile"
+	"github.com/kingsoftcloud/sdk-go/ksyun/common"
+	ksyunhttp "github.com/kingsoftcloud/sdk-go/ksyun/common/http"
+	"github.com/kingsoftcloud/sdk-go/ksyun/common/profile"
 )
 
 const APIVersion = "2015-11-01"
@@ -49,6 +49,39 @@ func (c *Client) AssumeRoleWithContext(ctx context.Context, request *AssumeRoleR
 	request.SetContentType("application/x-www-form-urlencoded")
 
 	response := NewAssumeRoleResponse()
+	err, msg := c.Send(request, response)
+	if err != nil {
+		return fmt.Sprintf("%+v\n", err)
+	}
+	return msg
+}
+func NewAssumeRoleTemporarySAMLRequest() (request *AssumeRoleTemporarySAMLRequest) {
+	request = &AssumeRoleTemporarySAMLRequest{
+		BaseRequest: &ksyunhttp.BaseRequest{},
+	}
+	request.Init().WithApiInfo("sts", APIVersion, "AssumeRoleTemporarySAML")
+	return
+}
+
+func NewAssumeRoleTemporarySAMLResponse() (response *AssumeRoleTemporarySAMLResponse) {
+	response = &AssumeRoleTemporarySAMLResponse{
+		BaseResponse: &ksyunhttp.BaseResponse{},
+	}
+	return
+}
+
+func (c *Client) AssumeRoleTemporarySAML(request *AssumeRoleTemporarySAMLRequest) string {
+	return c.AssumeRoleTemporarySAMLWithContext(context.Background(), request)
+}
+
+func (c *Client) AssumeRoleTemporarySAMLWithContext(ctx context.Context, request *AssumeRoleTemporarySAMLRequest) string {
+	if request == nil {
+		request = NewAssumeRoleTemporarySAMLRequest()
+	}
+	request.SetContext(ctx)
+	request.SetContentType("application/json")
+
+	response := NewAssumeRoleTemporarySAMLResponse()
 	err, msg := c.Send(request, response)
 	if err != nil {
 		return fmt.Sprintf("%+v\n", err)
--- ksyun/client/sts/v20151101/models.go
+++ ksyun/client/sts/v20151101/models.go
@@ -2,8 +2,8 @@ package v20151101
 
 import (
 	"encoding/json"
-	"github.com/kingsoftcloud/sdk-go/v2/ksyun/common/errors"
-	ksyunhttp "github.com/kingsoftcloud/sdk-go/v2/ksyun/common/http"
+	"github.com/kingsoftcloud/sdk-go/ksyun/common/errors"
+	ksyunhttp "github.com/kingsoftcloud/sdk-go/ksyun/common/http"
 )
 
 type AssumeRoleRequest struct {
@@ -53,5 +53,55 @@ func (r *AssumeRoleResponse) ToJsonString() string {
 }
 
 func (r *AssumeRoleResponse) FromJsonString(s string) error {
+	return json.Unmarshal([]byte(s), &r)
+}
+
+type AssumeRoleTemporarySAMLRequest struct {
+	*ksyunhttp.BaseRequest
+	RoleKrn         *string `json:"RoleKrn,omitempty" name:"RoleKrn"`
+	RoleSessionName *string `json:"RoleSessionName,omitempty" name:"RoleSessionName"`
+	DurationSeconds *string `json:"DurationSeconds,omitempty" name:"DurationSeconds"`
+	Policy          *string `json:"Policy,omitempty" name:"Policy"`
+}
+
+func (r *AssumeRoleTemporarySAMLRequest) ToJsonString() string {
+	b, _ := json.Marshal(r)
+	return string(b)
+}
+
+func (r *AssumeRoleTemporarySAMLRequest) FromJsonString(s string) error {
+	f := make(map[string]interface{})
+	if err := json.Unmarshal([]byte(s), &f); err != nil {
+		return err
+	}
+	if len(f) > 0 {
+		return errors.NewKsyunSDKError("ClientError.BuildRequestError", "AssumeRoleTemporarySAMLRequest has unknown keys!", "")
+	}
+	return json.Unmarshal([]byte(s), &r)
+}
+
+type AssumeRoleTemporarySAMLResponse struct {
+	*ksyunhttp.BaseResponse
+	AssumeRoleResult struct {
+		Credentials struct {
+			Expiration      *string `json:"Expiration" name:"Expiration"`
+			SecretAccessKey *string `json:"SecretAccessKey" name:"SecretAccessKey"`
+			AccessKeyId     *string `json:"AccessKeyId" name:"AccessKeyId"`
+			SecurityToken   *string `json:"SecurityToken" name:"SecurityToken"`
+		} `json:"Credentials" name:"Credentials"`
+		AssumedRoleUser struct {
+			Krn           *string `json:"Krn" name:"Krn"`
+			AssumedRoleId *string `json:"AssumedRoleId" name:"AssumedRoleId"`
+		} `json:"AssumedRoleUser" name:"AssumedRoleUser"`
+	} `json:"AssumeRoleResult"`
+	RequestId *string `json:"RequestId" name:"RequestId"`
+}
+
+func (r *AssumeRoleTemporarySAMLResponse) ToJsonString() string {
+	b, _ := json.Marshal(r)
+	return string(b)
+}
+
+func (r *AssumeRoleTemporarySAMLResponse) FromJsonString(s string) error {
 	return json.Unmarshal([]byte(s), &r)
 }
